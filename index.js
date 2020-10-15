faad3703af5493afa922b058a1d220df1a5947ec
ivanlewin <57846638+ivanlewin@users.noreply.github.com>
2020-10-15 01:17:48 -0300

    add logic to check the email against a regex

--- index.js
+++ index.js
@@ -12,5 +12,12 @@ console.log("asd")
 form.addEventListener("submit", function(e) {
     e.preventDefault()
     console.log("email: ", email.value)
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    
+    // Checks the email on the form against the RegExp found on the MDN website
+    // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/email#Basic_validation
+    const check = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(email.value);
+    
 })
 
