6c07748aeee4fbaa588a832bf121fc887f9a7f74
ivanlewin <57846638+ivanlewin@users.noreply.github.com>
2020-10-15 01:17:21 -0300

    deleted instructions

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-/* 
-PART 2: STRETCH GOAL
+const email = document.getElementById("email-input");
+const form = document.getElementById("newsletterForm");
 
-1. Validate that it's an email the user has entered
-2. Give feedback as to whether the form was successfully submitted or not
-3. Go crazy with the error/success feedback (e.g. animations, sounds)
 
 */
 
