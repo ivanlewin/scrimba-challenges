65873502903ce9275ad9359bd03531e77a95a035
ivanlewin <57846638+ivanlewin@users.noreply.github.com>
2020-10-16 13:01:09 -0300

    made the showFeedback function

--- WeeklyWebDevChallenge-20201016/index.js
+++ WeeklyWebDevChallenge-20201016/index.js
@@ -1,10 +1,46 @@
 const email = document.getElementById("email-input");
+const name = document.getElementById("name-input");
 const form = document.getElementById("newsletterForm");
 const modeToggler = document.getElementById("toggler");
+const emailAddress = document.getElementById("emailAddress");
+const modal = document.getElementById("modal");
+const overlay = document.getElementById("overlay");
+const nameSpan = document.getElementById("name");
 
 const showFeedback = function(feedback) {
+const showFeedback = function(success) {
     // Displays a modal with a success/error message
-    console.log(feedback);
+    if(success) {
+        confetti.start();
+        document.getElementById("feedback").textContent = "Hooray! 🎉";
+        console.log(name.value)
+        if(name.value != "") {
+            document.getElementById("feedback-msg").innerHTML = `<span id="name">Welcome aboard, ${name.value}</span>! We added <span id="emailAddress">${email.value}</span> to the mailing list! 📬`;
+        } else {
+            document.getElementById("feedback-msg").innerHTML = `We added <span id="emailAddress">${email.value}</span> to the mailing list! 📬`;
+            }
+        }
+    else {
+        if(name.value != "") {
+            document.getElementById("feedback-msg").innerHTML = `<span id="name">Sorry, ${name.value}</span>, we couldn't add <span id="emailAddress">your email</span> to the mailing list because it's not a valid address. Please try again.`
+        }
+        emailAddress.textContent = email.value;
+    }
+        
+    overlay.style.display = "block";
+    
+    document.getElementById("close-modal").addEventListener("click", function() {
+        overlay.style.display = "none";
+        confetti.stop();
+    })
+    
+    overlay.addEventListener("click", function(e) {
+        e.stopPropagation();
+        if(e.target == overlay) {
+            overlay.style.display = "none";
+            confetti.stop();
+        }
+    })
 }
 
 const toggleMode = function(event) {
